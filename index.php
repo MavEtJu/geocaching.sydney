2d059744f9b92f2014bf4c3b68a2c5a62eccc7c7
Edwin Groothuis <edwin@mavetju.org>
2015-12-24 10:12:39 +1100

    Initial checkin

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+html_header();
+html_intro();
+html_footer();
+
+
+function html_intro()
+{
+?>
+<img src="images/geocaching-logo-150x150.png" />
+<p>
+You are early, how did you end up here?
+<?php
+}
+
+
+function html_header()
+{
+?>
+    <html><header>
+    <title>Geocaching in Sydney</title>
+    </header><body>
+<?php
+}
+
+function html_footer()
+{
+?>
+    </body></html>
+<?php
+}
+
+?>
